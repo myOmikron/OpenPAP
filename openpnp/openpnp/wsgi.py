a50c1e4850fdcf0635bb3811bb2a74833c224347
BuildTools <unconfigured@null.spigotmc.org>
2020-05-06 13:04:47 +0200

    Reinitialized the project

--- openpnp/openpnp/wsgi.py
+++ openpnp/openpnp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OpenPAP project.
+WSGI config for openpnp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpenPAP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openpnp.settings')
 
 application = get_wsgi_application()
