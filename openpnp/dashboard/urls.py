ccf70b81cd2a0506a602ccb90dad65ca05cdf94b
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 03:47:21 +0200

    Added urls to dashboard, gamemaster and player sites

--- openpnp/dashboard/urls.py
+++ openpnp/dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from dashboard.views import IndexView
+from dashboard.views import *
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('gamemaster', GameMasterView.as_view()),
+    path('player', PlayerView.as_view()),
 ]
--- openpnp/dashboard/views.py
+++ openpnp/dashboard/views.py
@@ -6,4 +6,29 @@ class IndexView(TemplateView):
     template_name = "dashboard/index.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        return render(request, self.template_name, {"title": "OpenPnP - Dashboard",
+                                                    "dashboard_link_player": "/player",
+                                                    "dashboard_link_game_master": "/gamemaster",
+                                                    "dashboard_link_settings": "/admin"})
+
+
+class GameMasterView(TemplateView):
+    template_name = "dashboard/gamemaster.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {"title": "OpenPnP - GameMaster",
+                                                    "dashboard_link_create_campaign": "/create_campaign",
+                                                    "dashboard_link_create_map": "/create_map",
+                                                    "dashboard_link_create_monster": "/create_monster",
+                                                    "dashboard_link_create_encounter": "/create_encounter",
+                                                    "dashboard_link_create_npc": "/create_npc",
+                                                    "dashboard_link_create_world_setting": "/create_world_setting"})
+
+
+class PlayerView(TemplateView):
+    template_name = "dashboard/player.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {"title": "OpenPnP - Player",
+                                                    "dashboard_link_play": "/play",
+                                                    "dashboard_link_character": "/character"})
