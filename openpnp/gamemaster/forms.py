97e546736c7f1b28b442b9c72d748ac2511000ca
BuildTools <unconfigured@null.spigotmc.org>
2020-07-13 03:54:25 +0200

    Replaced CreateTemplateForm with CreateView, made name attribute in Template unique

--- openpnp/gamemaster/forms.py
+++ openpnp/gamemaster/forms.py
@@ -1,18 +1,2 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
-from gamemaster import models
-
-
-class CreateTemplateForm(forms.Form):
-    name = forms.CharField(max_length=255, min_length=1)
-
-    def clean_name(self):
-        if self.cleaned_data["name"] in [obj.name for obj in models.Template.objects.all()]:
-            raise ValidationError("Object already exists!")
-        return self.cleaned_data["name"]
-
-    def create_template(self):
-        temp = models.Template()
-        temp.name = self.cleaned_data["name"]
-        temp.save()
--- openpnp/gamemaster/models.py
+++ openpnp/gamemaster/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Template(models.Model):
-    name = models.CharField(default="", max_length=255)
+    name = models.CharField(default="", max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
